14e3dc35700b1619d7a66dbe102674104dba7c77
ashishthapa <acis.thapa@gmail.com>
2021-11-14 12:32:36 +0200

    Removes unnecessary files from index

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
 const express = require('express');
 const app = express();
 
+const todoController = require('./controllers/todo.controller');
 
-let todoList = [
-    {
-      id: 1,
-      task: "Brush Teeth",
-      Status: "Done"
-    },
-    {
-      id: 2,
-      task: "Eat Breakfast",
-      Status: "Done"
-    },
-    {
-        id: 3,
-        task: "Go To School",
-        Status: "Done"
-    },
-    {
-        id: 4,
-        task: "Return Home",
-        Status: "Done"
-      },
-      {
-          id: 5,
-          task: "Complete Assignment",
-          Status: "Pending"
-      }
-  ]
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Todo App!</h1>');
-});
-
+app.get('/', (todoController.getAllTodos))
   
-app.get('/api/v1/todo', (req, res) => {
-})
+app.get('/api/v1/todo', (todoController.getAllTodos))
 
-app.put('/api/v1/todo/:id', (req, res) => {
-
-});
+app.put('/api/v1/todo/:id', (todoController.updateTodo));
 
 const PORT = 3001
 app.listen(PORT);
