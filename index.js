677b314abd4e8100729b4a18691ce3a83f128b20
ashishthapa <acis.thapa@gmail.com>
2021-11-14 11:21:52 +0200

    Add Express && Add Mock-Data to test

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const http = require('http');
 
-
+let todoList = [
+    {
+      id: 1,
+      task: "Brush Teeth",
+      Status: "Done"
+    },
+    {
+      id: 2,
+      task: "Eat Breakfast",
+      Status: "Done"
+    },
+    {
+        id: 3,
+        task: "Go To School",
+        Status: "Done"
+    },
+    {
+        id: 4,
+        task: "Return Home",
+        Status: "Done"
+      },
+      {
+          id: 5,
+          task: "Complete Assignment",
+          Status: "Pending"
+      }
+  ]
 
 const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end("Tesing NodeJS for ToDoAP");
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(todoList))
 });
 
 const PORT = 3001
