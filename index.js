1a362319c1c55e4f80f51ae19d02baf733530450
ashishthapa <acis.thapa@gmail.com>
2021-11-14 17:00:41 +0200

    Add basic setup and test for Backend

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const app = express();
 
 const todoController = require('./controllers/todo.controller');
 
-app.get('/', (todoController.getAllTodos))
+app.get('/', (todoController.getAllTodos));
   
-app.get('/api/v1/todo', (todoController.getAllTodos))
+app.get('/api/v1/todo', (todoController.getAllTodos));
 
-app.put('/api/v1/todo/:id', (todoController.updateTodo));
+// app.put('/api/v1/todo/:id', (todoController.updateTodo));
 
 const PORT = 3001
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+module.exports = app;
