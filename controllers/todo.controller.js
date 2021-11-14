729fb489fda1e097cfb9ebc84f97c8e995999a79
ashishthapa <acis.thapa@gmail.com>
2021-11-14 13:08:59 +0200

    Clean up todo.controller file && Move todolist json to separate todoList.json file

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,35 +1,9 @@
-let todoList = [
-    {
-      id: 1,
-      task: "Brush Teeth",
-      Status: "Done"
-    },
-    {
-      id: 2,
-      task: "Eat Breakfast",
-      Status: "Done"
-    },
-    {
-        id: 3,
-        task: "Go To School",
-        Status: "Done"
-    },
-    {
-        id: 4,
-        task: "Return Home",
-        Status: "Done"
-      },
-      {
-          id: 5,
-          task: "Complete Assignment",
-          Status: "Pending"
-      }
-  ]
 
 exports.getAllTodos = (req, res) => {
     res.json(todoList);
+    res.send('Get all todo lists')
 }
 
 exports.updateTodo = (req, res) => {
-    res.send('Time to update the status');
+    res.send('Update the status of one todo list');
 }
