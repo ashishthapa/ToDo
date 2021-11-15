8e8cc0cfaeeabdd65ce77b74c169f2d9288c94e0
ashishthapa <acis.thapa@gmail.com>
2021-11-15 21:04:44 +0200

    Fixes backend logic

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -12,14 +12,19 @@ exports.getAllTodos = async (req, res) => {
 }
 
 exports.updateTodo = (req, res) => {
-    const id = req.params.id;
+    let id = Number(req.params.id);
     try {
         fs.readFile(path, (error, data) => {
             if (error) { console.log(error); return; }
             
             let parsedToDos = JSON.parse(data.toString());
-            parsedToDos[id - 1]['status'] = (parsedToDos[id - 1]['status'] === "Pending") ? "Done" : "Pending";
-            
+            console.log('parsed', parsedToDos);
+            console.log(typeof id);
+            let todoIndex = parsedToDos.findIndex((todo => todo.id === id));
+            console.log('todoindex', todoIndex);
+            if(todoIndex  < 0) {res.json('Index not found');}
+
+            parsedToDos[todoIndex]['status'] = parsedToDos[todoIndex]['status'] === "Pending" ? "Done" : "Pending";
             fs.writeFile(path, JSON.stringify(parsedToDos, null, 2), (err) => {
                 if (err) { console.log('Update Unsuccessful, Parsed todos: err', err); return; }
                 res.json(JSON.parse(JSON.stringify(parsedToDos)));
