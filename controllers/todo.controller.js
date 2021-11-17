47d311eb312524f4594e7b26a25c23db9de80f39
Ashish Thapa <ashishthapa@Frosmos-MacBook-Pro-104.local>
2021-11-17 20:16:52 +0200

    Remove comments

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = './todoList.json';
 
 
-exports.getAllTodos = async (req, res) => {
+exports.getAllTodos = async(req, res) => {
     try {
         fs.readFile(path, (error, data) => {
             if (error) { console.log(error); return; }
@@ -12,25 +12,16 @@ exports.getAllTodos = async (req, res) => {
 }
 
 exports.updateTodo = (req, res) => {
-    console.log('params', req.params);
-    console.log('body', req.body);
     let id = Number(req.params.id);
     try {
         fs.readFile(path, (error, data) => {
             if (error) { console.log(error); return; }
-            
             let parsedToDos = JSON.parse(data.toString());
-            console.log('parsed', parsedToDos);
             let todoIndex = parsedToDos.findIndex((todo => todo.id === id));
-            console.log('todoindex', todoIndex);
-            if(todoIndex  < 0) {res.json('Index not found'); return; }
-
-            // parsedToDos[todoIndex]['status'] = parsedToDos[todoIndex]['status'] === "Pending" ? "Done" : "Pending";
+            if (todoIndex < 0) { res.json('Index not found'); return; }
             parsedToDos[todoIndex] = req.body;
-            console.log('Parsed looks like this', parsedToDos);
             fs.writeFile(path, JSON.stringify(parsedToDos, null, 2), (err) => {
                 if (err) { console.log('Update Unsuccessful, Parsed todos: err', err); return; }
-                // res.json(JSON.parse(JSON.stringify(parsedToDos)));
                 res.json(JSON.parse(JSON.stringify(req.body)));
             });
         })
@@ -39,4 +30,3 @@ exports.updateTodo = (req, res) => {
         throw err;
     }
 }
-
