baa48b8b83f008f05af723a433259ccf4d82eef5
Ashish Thapa <ashishthapa@Frosmos-MacBook-Pro-104.local>
2021-11-17 23:16:10 +0200

    Missing commits

--- todo-client/cypress/integration/todo.spec.js
+++ todo-client/cypress/integration/todo.spec.js
@@ -5,18 +5,23 @@ describe('to do', function () {
         cy.get('.li-todo').should('have.length', 5)
         cy.get('.li-todo').eq(0).contains(11)
 
-
-        cy.get('li-todo').contains('td', 'Done').then(elem => {
+        cy.get('td').eq(4).contains('Done').then(elem => {
             // Do something with this specific element...
-            cy.get('.li-todo ').eq(4).click()
-            cy.get('.li-todo ').eq(4).contains('Done')
+            cy.get('.li-todo status').eq(4).click()
+            cy.get('.li-todo ').eq(4).contains('Pending')
         });
 
-        cy.get('.li-todo').eq(4).contains('td', 'Pending').then(elem => {
-            // Do something with this specific element...
-            cy.get('.li-todo ').eq(4).click()
-            cy.get('.li-todo ').eq(4).contains('Done')
-        });
+        // cy.get('.li-todo').eq(4).contains('td').then(elem => {
+        //     // Do something with this specific element...
+        //     cy.get('.li-todo status').eq(4).click()
+        //     cy.get('.li-todo ').eq(4).contains('Pending')
+        // });
+
+        // cy.get('.li-todo').eq(4).contains('td', 'Pending').then(elem => {
+        //     // Do something with this specific element...
+        //     cy.get('.li-todo .status').eq(4).click()
+        //     cy.get('.li-todo ').eq(4).contains('Done')
+        // });
 
         // cy.get('.li-todo ').eq(4).click()
         // cy.get('.li-todo ').eq(4).contains('Done')
