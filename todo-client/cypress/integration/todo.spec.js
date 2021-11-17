aea7b252b4ebdb8150314ddfc3212604a588e81e
Ashish Thapa <ashishthapa@Frosmos-MacBook-Pro-104.local>
2021-11-17 19:16:39 +0200

    Add delay to Cypress test

--- todo-client/cypress/integration/todo.spec.js
+++ todo-client/cypress/integration/todo.spec.js
@@ -1,6 +1,7 @@
 describe('to do', function () {
     it('front page loaded with all the items', function () {
         cy.visit('http://localhost:3000')
+        cy.wait(5000);
         cy.get('.li-todo').should('have.length', 5)
         cy.get('.li-todo').eq(0).contains(11)
 
